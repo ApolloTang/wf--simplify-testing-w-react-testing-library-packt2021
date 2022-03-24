c3cb50abfb55a2b7f8f64c0be6350586e75df69f
Apollo Tang <tang.apollo@gmail.com>
2022-03-24 01:40:53 -0400

    add note on enzyme for react 17

--- b08/test-setup/jest.init.js
+++ b08/test-setup/jest.init.js
@@ -1,7 +1,7 @@
 // This file will run on each test, before jest is loaded
 
 const Enzyme = require('enzyme');
-const Adapter = require('@wojtekmaj/enzyme-adapter-react-17');
+const Adapter = require('@wojtekmaj/enzyme-adapter-react-17'); // https://github.com/enzymejs/enzyme/issues/2429#issuecomment-679265564
 
 Enzyme.configure({ adapter: new Adapter() });
 
