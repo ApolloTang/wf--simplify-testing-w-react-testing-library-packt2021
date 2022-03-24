20390d482eb95f3ca0b53b407b8a448f99bce6f3
Apollo Tang <tang.apollo@gmail.com>
2022-03-24 01:57:18 -0400

    c01 load component

--- b08/src/00/c01/App.js
+++ b08/src/00/c01/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Login from './Login';
+import Profile from './Profile';
 
-const App = () => <h1>Replace me with chapter related components </h1>;
+// const App = () => <Login />;
+const App = () => <Profile />;
 export default App;
